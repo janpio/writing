265611e0221bc9d5b5fcdb4389bdece090b20598
Julius de Bruijn <julius+github@nauk.io>
2022-01-27 10:35:31 +0100

    use github markdown format

--- convert.sh
+++ convert.sh
@@ -4,5 +4,5 @@ for file in **/*.org; do
   echo $file
   name=$(echo "$file" | cut -f 1 -d '.')
   pandoc -s "$file" -o "$name".pdf --pdf-engine=lualatex
-  pandoc -s "$file" -o "$name".md
+  pandoc -t gfm -s "$file" -o "$name".md
 done
